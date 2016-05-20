nathanmarks/material-ui

[StepContent][ExpandTransition] Add transition props to StepContent so the animation is configurable, improve integration test

src/Stepper/StepContent.js
@@ -1,6 +1,10 @@
 import React, {Component, PropTypes} from 'react';
-import ExpandTransition from '../internal/ExpandTransition';
+import TransitionComponent from '../internal/ExpandTransition';
 import warning from 'warning';
+
+function ExpandTransition(props) {
+  return <TransitionComponent {...props} />;
+}
 
 const getStyles = (props, context) => {
   const styles = {
@@ -38,6 +42,19 @@ class StepContent extends Component {
      * Override the inline-style of the root element.
      */
     style: PropTypes.object,
+    /**
+     * ReactTransitionGroup component.
+     */
+    transition: PropTypes.func,
+    /**
+     * Adjust the duration of the content expand transition. Passed as a prop to the transition component.
+     */
+    transitionDuration: PropTypes.number,
+  };
+
+  static defaultProps = {
+    transition: ExpandTransition,
+    transitionDuration: 450,
   };
 
   static contextTypes = {
@@ -51,6 +68,8 @@ class StepContent extends Component {
       children,
       last, // eslint-disable-line no-unused-vars
       style,
+      transition,
+      transitionDuration,
       ...other,
     } = this.props;
     const {stepper, muiTheme: {prepareStyles}} = this.context;
@@ -61,12 +80,15 @@ class StepContent extends Component {
     }
 
     const styles = getStyles(this.props, this.context);
+    const transitionProps = {
+      transitionDelay: transitionDuration,
+      transitionDuration: transitionDuration,
+      open: active,
+    };
 
     return (
       <div style={prepareStyles(Object.assign(styles.root, style))} {...other}>
-        <ExpandTransition enterDelay={450} open={active}>
-          <div style={{overflow: 'hidden'}}>{children}</div>
-        </ExpandTransition>
+        {React.createElement(transition, transitionProps, <div style={{overflow: 'hidden'}}>{children}</div>)}
       </div>
     );
   }

stoked-ui/sui

src/Stepper/StepContent.js
@@ -1,6 +1,10 @@
 import React, {Component, PropTypes} from 'react';
-import ExpandTransition from '../internal/ExpandTransition';
+import TransitionComponent from '../internal/ExpandTransition';
 import warning from 'warning';
+
+function ExpandTransition(props) {
+  return <TransitionComponent {...props} />;
+}
 
 const getStyles = (props, context) => {
   const styles = {
@@ -38,6 +42,19 @@ class StepContent extends Component {
      * Override the inline-style of the root element.
      */
     style: PropTypes.object,
+    /**
+     * ReactTransitionGroup component.
+     */
+    transition: PropTypes.func,
+    /**
+     * Adjust the duration of the content expand transition. Passed as a prop to the transition component.
+     */
+    transitionDuration: PropTypes.number,
+  };
+
+  static defaultProps = {
+    transition: ExpandTransition,
+    transitionDuration: 450,
   };
 
   static contextTypes = {
@@ -51,6 +68,8 @@ class StepContent extends Component {
       children,
       last, // eslint-disable-line no-unused-vars
       style,
+      transition,
+      transitionDuration,
       ...other,
     } = this.props;
     const {stepper, muiTheme: {prepareStyles}} = this.context;
@@ -61,12 +80,15 @@ class StepContent extends Component {
     }
 
     const styles = getStyles(this.props, this.context);
+    const transitionProps = {
+      enterDelay: transitionDuration,
+      transitionDuration: transitionDuration,
+      open: active,
+    };
 
     return (
       <div style={prepareStyles(Object.assign(styles.root, style))} {...other}>
-        <ExpandTransition enterDelay={450} open={active}>
-          <div style={{overflow: 'hidden'}}>{children}</div>
-        </ExpandTransition>
+        {React.createElement(transition, transitionProps, <div style={{overflow: 'hidden'}}>{children}</div>)}
       </div>
     );
   }