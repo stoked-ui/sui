e329ccf3038106137ba536df36867d3aa9b40cdb
Brian Stoker <b@stokedconsulting.com>
2024-09-02 20:34:10 -0500

    removed core for now

--- docs/pages/core/index.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import Home from "docs/pages";
-import Main from "./main";
-
-export default function Page() {
-  return <Home HomeMain={Main} />;
-}
--- packages/sui-core/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    'import/prefer-default-export': 'off',
-    'react-hooks/exhaustive-deps': 'warn',
-    'import/no-cycle': 'error',
-    'no-underscore-dangle': 'off',
-  },
-};
