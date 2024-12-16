128fcc61b639b425c224f8443e117052b783debb
Brian Stoker <b@stokedconsulting.com>
2024-12-15 21:54:03 -0600

    remove sr tool

--- packages-internal/sui-search-replace/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    'import/prefer-default-export': 'off',
-    'no-await-in-loop': 'off',
-    'no-restricted-syntax': 'off',
-    'no-console': 'off',
-  },
-};
--- packages-internal/sui-search-replace/README.md
+++
@@ -1,3 +0,0 @@
-search and replace text in files, file names, and directories recursively from a directory starting point
-
-sr /path/to/directory "oldText" "newText"
