dc44c7e0e9e720099e03db5298d796f01766e1b9
brian-stoker <b@stokedconsulting.com>
2025-02-18 19:41:28 -0600

    updated sst deploy

--- infra/domains.ts
+++ infra/domains.ts
@@ -1,7 +1,7 @@
 
 const getSubdomains = () => {
   const stage = $app.stage;
-  if (stage === 'prod') {
+  if (stage === 'production') {
     return [`stoked-ui.com`, `www.stoked-ui.com`, `www.stokedconsulting.com`, `stokedconsulting.com`];
   }
   return [`${stage}.stoked-ui.com`, `*.${stage}.stoked-ui.com`, `*.${stage}.stokedconsulting.com`, `${stage}.stokedconsulting.com`];
