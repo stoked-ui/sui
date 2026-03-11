235e709ca9ed928e096f9ed58d780259dc3c7556
Brian Stoker <b@stokedconsulting.com>
2026-03-11 15:38:34 -0500

    chore: remove telegram deploy secrets

--- infra/secrets.ts
+++ infra/secrets.ts
@@ -8,11 +8,3 @@ export const stripeWebhookSecret = new sst.Secret(
 export const jwtSecret = new sst.Secret('JWT_SECRET');
 export const blogApiToken = new sst.Secret('BLOG_API_TOKEN');
 export const invoiceApiKey = new sst.Secret('INVOICE_API_KEY');
-export const telegramBotToken = new sst.Secret(
-  'TELEGRAM_BOT_TOKEN',
-  process.env.TELEGRAM_BOT_TOKEN,
-);
-export const telegramSupportChatId = new sst.Secret(
-  'TELEGRAM_SUPPORT_CHAT_ID',
-  process.env.TELEGRAM_SUPPORT_CHAT_ID,
-);
